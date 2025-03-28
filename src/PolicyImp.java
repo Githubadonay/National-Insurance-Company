d557d5ffe3d365868105c4d64dd1cc31e6d82d50
adonayaraia02@gmail.com <adonayaraia02@gmail.com>
2025-03-27 21:45:50 -0400

    added a switch case

--- src/PolicyImp.java
+++ src/PolicyImp.java
@@ -4,9 +4,23 @@ public class PolicyImp {
     public static void main(String[] args){
         Policy[] allPolicies = new Policy[100];
 
-        int choice = Integer.parseInt(JOptionPane.showInputDialog("1. Add Policies\n2.Remove Policies\n3.View Policies\n4.Quit"))
+        int input = -1;
+         
+        do {
+            input = Integer.parseInt(JOptionPane.showInputDialog("1. Add Policies\n2.Remove Policies\n3.View Policies\n4.Quit"));
+        switch(input){
+            case 1:
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            default:
 
+         }
+    } while(input >= 1 && input <= 4);
 
-    }
-    
+}
 }
